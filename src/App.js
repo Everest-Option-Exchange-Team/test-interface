172d72b86e4451a6431d72c5819b094b88ee0ba3
Linus Weigand <linusweigand@protonmail.com>
2022-04-13 22:07:51 +0200

    checkIfWalletIsConnected

--- src/App.js
+++ src/App.js
@@ -8,7 +8,28 @@ export default function App() {
   const contractAddress = "";
 
   const checkIfWalletIsConnected = async () => {
-    
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure  you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_accounts'});
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account: ", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   const connectWallet = () => {
