b56282a506d9eab49174135c6f257792e73c8219
Linus Weigand <56792439+LinusWeigand@users.noreply.github.com>
2022-04-20 16:55:38 +0200

    Fund functionality (#6)
    * yarn.lock resolve
    
    * added react-numeric-input
    
    * added Numeric Input
    
    * added deposit button
    
    * added withdraw input & button
    
    * 0.1 step
    
    * updateAmountFunded
    
    * updateAmountFunded Button
    
    * connect Buttons with methods
    
    * updated contract
    
    * updateAmountFunded
    
    * updated contract
    
    * updateAmountFunded
    
    * deposit
    
    * withdraw
    
    * deposit
    
    * installed bignumber.js
    
    * deposit
    
    * deposit
    
    * BigNumber troubleshoot
    
    * BigNumber fixup
    
    * from git gui
    
    * connect Input with function
    
    * finish up
    
    * utils -> abis
    
    * formatAvax
    
    * update on refresh
    
    * update on refresh
    
    * update on refresh
    
    * display & update totalAmountFunded
    
    * fixup
    
    * fixed react Hooks
    
    * added currency symbol AVAX
    
    * test
    
    Co-authored-by: Linus Weigand <linusweigand@couchtec.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { ethers } from "ethers";
+import React, { useState, useEffect, useCallback } from "react";
+import NumericInput from "react-numeric-input";
+import { ethers, BigNumber } from "ethers";
 import './App.css';
-import abi from './utils/Fund.json';
+import abi from './abis/Fund.json';
 
 export default function App() {
   const [currentAccount, setCurrentAccount] = useState("");
   const [isCurrentlyConnected, setCurrentlyConnected] = useState(false);
-  const contractAddress = "0x479E9da1f7a5a1Ae2EBD7bCBd05958107485afb6";
+  const contractAddress = "0x5081f431918Ccc62DBDfaCBc11f34B4166A27450";
   const contractABI = abi.abi;
+  const [amountFunded, setAmountFunded] = useState(BigNumber.from('0'));
+  const [amountDeposit, setAmountDeposit] = useState(0);
+  const [amountWithdraw, setAmountWithdraw] = useState(0);
+  const [totalAmountFunded, setTotalAmountFunded] = useState(BigNumber.from('0'));
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -54,9 +59,90 @@ export default function App() {
     }
   }
 
+  const deposit = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const fundContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        await fundContract.fund({ value: ethers.utils.parseEther(amountDeposit.toString())});
+        updateAmountFunded();
+      } else {
+        console.log("Ethereum object doesn't exist");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const withdraw = async () => {
+    updateAmountFunded();
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const fundContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        fundContract.withdraw(ethers.utils.parseEther(amountWithdraw.toString()));
+      } else {
+        console.log("Ethereum object doesn't exist");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const updateAmountFunded = useCallback(async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const fundContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        setAmountFunded(BigNumber.from((await fundContract.getAddressToAmountFunded(currentAccount)).toHexString()));
+        
+      } else {
+        console.log("Ethereum object doesn't exist");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [contractABI, currentAccount]);
+
+  const updateTotalAmountFunded = useCallback(async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const fundContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        setTotalAmountFunded(BigNumber.from((await fundContract.getTotalFunds()).toHexString()));
+        
+      } else {
+        console.log("Ethereum object doesn't exist");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [contractABI]);
+
+  const formatAvax = (bigNumber) => {
+    return ethers.utils.formatEther(bigNumber);
+  }
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
+
+  useEffect(() => {
+    updateAmountFunded();
+    updateTotalAmountFunded();
+  }, [currentAccount, amountFunded, updateTotalAmountFunded, updateAmountFunded]);
 
 
   
@@ -69,7 +155,7 @@ export default function App() {
         </div>
 
         <div className="bio">
-          Fuji!!
+          Funds of contract: {formatAvax(totalAmountFunded).toString()} AVAX
         </div>
 
         { isCurrentlyConnected ? 
@@ -80,7 +166,18 @@ export default function App() {
               Connect Wallet
         </button>)
         }
-        
+        <div>
+          <button onClick={updateTotalAmountFunded}> Update amount Funded</button>
+          Avalanche Funded: {formatAvax(amountFunded).toString()} AVAX
+        </div>
+        <div>
+          <NumericInput min={0} value={amountDeposit} step={0.1} onChange={valueAsNumber => {setAmountDeposit(valueAsNumber)}}/>
+          <button onClick={deposit}>Deposit</button>
+        </div>
+        <div>
+          <NumericInput min={0} value={amountWithdraw} step={0.1} onChange={valueAsNumber => {setAmountWithdraw(valueAsNumber)}}/>
+          <button onClick={withdraw}>Withdraw</button>
+        </div>
       </div>
     </div>
   );
